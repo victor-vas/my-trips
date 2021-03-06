fc3c838cfb89c3119b3ec95326f9f815b6c12490
victor-vas <victorvasconcelos88@gmail.com>
2021-03-06 12:36:14 -0300

    config GraphQL client and refactored about page to dynamic pages

---
+++ src/templates/Pages/index.tsx
@@ -0,0 +1,21 @@
+import LinkWrapper from 'components/LinkWrapper';
+import { CloseOutline } from '@styled-icons/evaicons-outline';
+
+import * as S from './styles';
+
+export type PageTemplateProps = {
+  heading: string;
+  body: string;
+};
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+    <S.Heading>{heading}</S.Heading>
+    <S.Body dangerouslySetInnerHTML={{ __html: body }} />
+  </S.Content>
+);
+
+export default PageTemplate;
